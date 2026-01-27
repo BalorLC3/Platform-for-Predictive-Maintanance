5aa12dd90ab5519192b5147c24d548db945f0f26
Balor.LC3 <supernova20315@gmail.com>
2026-01-26 20:03:08 -0700

    Changed print to use logging

--- src/model/predict.py
+++ src/model/predict.py
@@ -1,11 +1,12 @@
 import torch
-import torch.nn as nn
-import numpy as np
 import pandas as pd
 from pathlib import Path
 from src.preprocessing.processor import DataProcessor, FEATURE_COLS
 from src.model.architecture import get_device, LSTMRegressor
-    
+import logging
+logger = logging.getLogger(__name__)
+
+
 class ModelPredictor:
     def __init__(
             self, 
@@ -14,12 +15,17 @@ class ModelPredictor:
     ):
         self.model_path = model_path
         self.device = get_device(device)
-        # load entire checkpoint
-        checkpoint = torch.load(
-            self.model_path, 
-            map_location=self.device, 
-            weights_only=True
-        )
+        # Load entire checkpoint
+        try:
+            checkpoint = torch.load(
+                self.model_path, 
+                map_location=self.device, 
+                weights_only=True
+                # Load weights only to avoid executing arbitrary code from checkpoint
+            )
+        except Exception as e:
+            logger.critical("Failed to load model checkpoint", exc_info=True)
+            raise RuntimeError("Model initialization failed") from e
         
         self.model = LSTMRegressor(
             input_dim=checkpoint['input_dim'],
@@ -41,11 +47,16 @@ class ModelPredictor:
         self.processor = DataProcessor(
             feature_cols=FEATURE_COLS
         )
-        print("Model and processor loaded successfully.")
 
     def predict(self, raw_data_df: pd.DataFrame) -> float:
         '''Takes raw data of a single engine's history and returns a RUL prediction'''
 
+        if raw_data_df.empty:
+            raise ValueError("Input data is empty")
+        # Despite of using schemas and previous validation, this is added as fallback
+        if list(raw_data_df.columns) != FEATURE_COLS:
+            raise ValueError("Input features do not match expected schema")
+        
         sequence = self.processor.transform(raw_data_df)
         sequence_tensor = torch.tensor(
             sequence,
